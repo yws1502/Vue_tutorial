de5636e3f2c2d1d40ba05c1c9ec0095909f53ebc
woosang0430 <yws1502@naver.com>
2022-04-05 16:52:04 +0900

    💄 게시판 UI 작업

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,61 @@
 <template>
-    <div>
-        This is Home
+    <div class="container">
+        <h2>게시판</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>제목</th>
+                    <th>내용</th>
+                    <th>댓글 수</th>
+                    <th>등록일</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr @click="clicked">
+                    <td>title</td>
+                    <td>content</td>
+                    <td>count</td>
+                    <td>created at</td>
+                </tr>
+            </tbody>
+        </table>
+        <button type="button">게시글 작성</button>
     </div>
 </template>
 
 <script>
 export default {
-
+    methods: {
+        clicked() {
+            console.log("clicked")
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+.container {
+    text-align: center;
+}
+h2 {
+    font-size: 25px;
+    font-weight: 550;
+    margin-bottom: 30px;
+}
+table {
+    margin: 0 auto 10px;
+}
+table th {
+    font-size: 18px;
+    font-weight: 550;
+}
+table th,
+table td {
+    padding: 8px;
+}
+tbody tr:hover {
+    cursor: pointer;
+    color: pink;
+    transition: .4s ease-out;
+}
 </style>
